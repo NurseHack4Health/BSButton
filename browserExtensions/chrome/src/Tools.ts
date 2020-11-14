751ecbc30d020020207cc199d655767d138bac1e
Alex Simons <alexsimons9999@gmail.com>
2020-11-14 08:06:36 -0600

    Added context menu reporting

--- browserExtensions/chrome/src/Tools.ts
+++ browserExtensions/chrome/src/Tools.ts
@@ -1,3 +1,3 @@
 export const testTool = () => {
-  console.log("hello world!")
+  console.log("hello world!!")
 }
--- browserExtensions/chrome/src/background.ts
+++ browserExtensions/chrome/src/background.ts
@@ -10,3 +10,12 @@ chrome.tabs.onUpdated.addListener(((tabId, changeInfo, tab) => {
     testTool();
   }
 }));
+
+chrome.contextMenus.create({
+  title: 'Report Covid-19 B.S.',
+  contexts: ['link', 'selection'],
+  onclick: function(e){
+    console.log('hey you did something', e);
+  }
+
+}, function(){})
